cf12422837a92bf3663153576b7d752d6edd1efe
aizhan04 <83398056+aizhan04@users.noreply.github.com>
2024-01-21 22:53:09 +0600

    Revert "Sprint 2"

--- src/components/icons/arrow/index.ts
+++ src/components/icons/arrow/index.ts
@@ -1,1 +1,2 @@
-export { default as ArrowRight } from "./arrow.hbs?raw";
+export {default as ArrowRight} from './arrow.hbs?raw'
+
--- src/pages/error404/index.ts
+++ src/pages/error404/index.ts
@@ -1,1 +1,1 @@
-export { default as Error404Page } from "./error404.hbs?raw";
+export {default as Error404Page} from './error404.hbs?raw';
