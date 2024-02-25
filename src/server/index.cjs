375da96f5c0365489a405a6715ec483e50251e4f
makhmutzhanova <makhmutzhanova@kolesa.kz>
2024-02-25 17:22:02 +0600

    sprint_3 add get in index.cjs

--- src/server/index.cjs
+++ src/server/index.cjs
@@ -5,7 +5,9 @@ const app = express()
 const PORT = 3000
 
 app.use(express.static(path.join(__dirname, '../../dist/')))
-
+app.get('*', (_req, res) => {
+    res.sendFile(path.join(__dirname, '../../dist/', 'index.html'));
+});
 app.listen(PORT, () => {
     console.log('Server is running on port ' + PORT)
 })
